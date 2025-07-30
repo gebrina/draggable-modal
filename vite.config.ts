8e149ce520e2d4fc109a0de9753866de2ee3ae76
gebrina <gsonofmary@gmail.com>
2025-07-30 23:13:15 +0300

    fix 'test' is not defined error on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfigs = defineViteConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@cpts": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@common": path.resolve(__dirname, "src/common"),
     },
   },
+});
+
+const vitestConfigs = defineVitestConfig({
   test: {
-    globals: true,
     environment: "jsdom",
+    globals: true,
   },
 });
+
+export default mergeConfig(viteConfigs, vitestConfigs);
